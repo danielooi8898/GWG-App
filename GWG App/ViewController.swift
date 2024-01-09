084918e040d3247ef67ab1572cf58c6447bd4b44
Mun Hong Ooi <mooi0007@student.monash.edu>
2024-01-09 12:55:17 +1100

    Initial Commit

---
+++ GWG App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GWG App
+//
+//  Created by Min En  on 9/1/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
